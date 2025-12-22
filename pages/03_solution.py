abb839734c838a3f3ef8577580bb291835c3c487
Jie-Yan094 <jarita0904@gmail.com>
2025-12-22 15:16:02 +0000

    modified:   pages/03_solution.py

--- pages/03_solution.py
+++ pages/03_solution.py
@@ -23,11 +23,11 @@ coral_display_type = solara.reactive("硬珊瑚")
 # 這裡要放照片-珊瑚礁
 coral_data = {
     "2019 健康珊瑚礁": {
-        "img": "https://huggingface.co/jarita094/starfish-assets/resolve/main/before_cots.jpg",
+        "img": "https://huggingface.co/jarita094/starfish-assets/resolve/main/starfish.jpg",
         "desc": "2019 健康珊瑚礁"
     },
     "2021 死亡珊瑚礁": {
-        "img": "https://huggingface.co/jarita094/starfish-assets/resolve/main/after_restoration.jpg",
+        "img": "https://huggingface.co/jarita094/starfish-assets/resolve/main/starfish.jpg",
         "desc": "2021 死亡珊瑚礁(藻類附著)"
     }
 }
@@ -76,12 +76,12 @@ def Page():
                     solara.Markdown("##### **A. 物理移除 : 人工夾取**")
                     solara.Markdown("* 臺灣因成本考量、技術上限制，需由專業潛水員使用長夾將海星移入網袋帶回岸上處理，但效率低且人力消耗大。")
                     # 這裡要放照片-夾海星
-                    solara.Image("https://huggingface.co/jarita094/starfish-assets/resolve/main/manual_removal.jpg", width="100%", style={"border-radius": "10px"})
+                    solara.Image("https://huggingface.co/jarita094/starfish-assets/resolve/main/starfish.jpg", width="100%", style={"border-radius": "10px"})
                     
                     solara.Markdown("##### **B. 生物化學：醋酸注射法**")
                     solara.Markdown("* **優點：** 效率高、不需帶回岸上、不會引發海星斷肢再生。\n* **方法：** 使用注射槍將15%醋酸注入海星體內，其殘骸會自然分解回歸生態鏈。")
                     # 這裡要放照片-注射海星
-                    solara.Image("https://huggingface.co/jarita094/starfish-assets/resolve/main/chemical_removal.jpg", width="100%", style={"border-radius": "10px"})
+                    solara.Image("https://huggingface.co/jarita094/starfish-assets/resolve/main/starfish.jpg", width="100%", style={"border-radius": "10px"})
                 
                 with solara.Column(style={"flex": "1", "min-width": "450px"}):
                     solara.Markdown("#### 🌿 珊瑚復育技術")
@@ -97,19 +97,19 @@ def Page():
                     在鎖港杭灣打造人工珊瑚礁生態系，利用軸孔珊瑚等進行無性繁殖與移植，形成水下「花園」，復育豐富的海洋生物，同時結合海洋教育和在地潛水業者，發展生態旅遊。
                     """)
                     # 這裡要放照片-種珊瑚
-                    solara.Image("https://huggingface.co/jarita094/starfish-assets/resolve/main/coral_planting.jpg", width="100%", style={"border-radius": "10px"})
+                    solara.Image("https://huggingface.co/jarita094/starfish-assets/resolve/main/starfish.jpg", width="100%", style={"border-radius": "10px"})
 
         # --- 4. 海洋廢棄物清理區塊 ---
         with solara.Card("🗑️ 行動三：海洋廢棄物清理 "):
             solara.Markdown(f"#### 海洋廢棄物統計資訊: [點此連結]({url})")
             # 這裡要放照片-海洋垃圾圖表_來不及做
-            solara.Image("https://huggingface.co/jarita094/starfish-assets/resolve/main/coral_planting.jpg", width="100%", style={"border-radius": "10px"})
+            solara.Image("https://huggingface.co/jarita094/starfish-assets/resolve/main/starfish.jpg", width="100%", style={"border-radius": "10px"})
             
             solara.Markdown("#### 海洋廢棄物治理計畫")
             solara.Markdown("除了因季風帶來的海洋垃圾問題之外，過度的捕撈，廢棄漁網會覆蓋珊瑚導致其死亡，並纏繞海龜等生物。")
             solara.Markdown("#### 相關報導")
             # 這裡要放照片
-            solara.Image("https://huggingface.co/jarita094/starfish-assets/resolve/main/coral_planting.jpg", width="100%", style={"border-radius": "10px"})
+            solara.Image("https://huggingface.co/jarita094/starfish-assets/resolve/main/starfish.jpg", width="100%", style={"border-radius": "10px"})
             solara.Markdown("* [綠色和平於澎湖海域清出約 400 公斤廢網](https://www.greenpeace.org/taiwan/press/32491/%E7%B6%A0%E8%89%B2%E5%92%8C%E5%B9%B3%E6%96%BC%E6%BE%8E%E6%B9%96%E6%B5%B7%E5%9F%9F%E6%B8%85%E5%87%BA%E7%B4%84-400-%E5%85%AC%E6%96%A4%E5%BB%A2%E7%B6%B2-%E4%BF%9D%E8%AD%B7%E5%8D%80%E6%B5%B7%E6%B4%8B/)")
         
         # --- 頁尾 ---
